f65d83d816f5baaa11651735bc263014ec561a40
sakshith15 <sakshith7777@gmail.com>
2020-02-21 01:20:42 +0530

    commit

---
+++ task_4/src/main/java/com/vnrvjiet/sakshith/task_4/SI_CI/CleanCode.java
@@ -0,0 +1,15 @@
+package com.vnrvjiet.sakshith.task_4.SI_CI;
+
+import java.util.*;
+//import java.lang.*;
+import java.io.*;
+
+
+public class CleanCode {
+	public static void main(String []args) {
+		simple_compound obj = new simple_compound();
+		obj.calculateInterest();
+		House_price obj1 = new House_price();
+		obj1.costCalculation();
+	}
+}
